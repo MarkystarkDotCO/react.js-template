d335826623921cf54ca29b9abb5da7d7e8c4f201
methanon <meayrek17577@gmail.com>
2022-08-06 04:48:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Home  from './pages/Home';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <Router>
+    <Routes>
+        <Route path='/'  element={<Home />} />
+        <Route path='/page2'  element={<h1>page2</h1>} />
+        <Route path='/page3'  element={<h1>page3</h1>} />
+
+    </Routes>
+    </Router>
+</div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import Footer from '../components/Footer';
+import Navbar1 from '../components/Navbar1';
+//import Brand from '../components/Brand';
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar1></Navbar1>
+      <h1>Welcome to GeeksforGeeks</h1>
+    </div>
+  );
+};
+  
+export default Home;
